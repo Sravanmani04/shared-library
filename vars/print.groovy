2efe066ffe96e9dfa40e3d75b7f6507f85f37d8b
Sravani1203 <ankireddypallisravani12@gmail.com>
2024-12-19 06:20:34 +0530

    b and q

--- vars/print.groovy
+++ vars/print.groovy
@@ -1,3 +1,11 @@
 def print(x,y){
     println "printing of ${x} and ${y}"
 }
+
+def printb(x,y){
+    println ("printing of ${x} and ${y}")
+}
+
+def printq(x,y){
+    println ("printing of ${x} and ${y}");
+}
